f1a3c536fdecd9973bfa2b2f3c598889a1c80e6b
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-10-16 04:13:23 +0300

    Setup themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -1,9 +1,88 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import * as Color from 'color';
+import { Storage } from '@ionic/storage';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private storage: Storage
+  ) {
+    storage.get('theme').then(cssText => {  // <--- GET SAVED THEME
+      this.setGlobalCSS(cssText);
+    });
+  }
+
+  // Override all global variables with a new theme
+  setTheme(theme) {
+    const cssText = CSSTextGenerator(theme);
+    this.setGlobalCSS(cssText);
+  }
+
+  // Define a single CSS variable
+  setVariable(name, value) {
+    this.document.documentElement.style.setProperty(name, value);
+  }
+
+  private setGlobalCSS(css: string) {
+    this.document.documentElement.style.cssText = css;
+  }
+}
+
+const defaults = {
+  primary: '#3880ff',
+  secondary: '#0cd1e8',
+  tertiary: '#7044ff',
+  success: '#10dc60',
+  warning: '#ffce00',
+  danger: '#f04141',
+  dark: '#222428',
+  medium: '#989aa2',
+  light: '#f4f5f8'
+};
+
+function contrast(color, ratio = 0.8) {
+  color = Color(color);
+  return color.isDark() ? color.lighten(ratio) : color.darken(ratio);
+}
+
+function CSSTextGenerator(colors) {
+  colors = { ...defaults, ...colors };
+
+  const {
+    primary,
+    secondary,
+    tertiary,
+    success,
+    warning,
+    danger,
+    dark,
+    medium,
+    light
+  } = colors;
+
+  const shadeRatio = 0.1;
+  const tintRatio = 0.1;
+
+  return `
+    --ion-color-base: ${light};
+    --ion-color-contrast: ${dark};
+    --ion-background-color: ${light};
+    --ion-text-color: ${dark};
+    --ion-toolbar-background-color: ${contrast(light, 0.1)};
+    --ion-toolbar-text-color: ${contrast(dark, 0.1)};
+
+    --ion-color-primary: ${primary};
+    --ion-color-primary-rgb: 56,128,255;
+    --ion-color-primary-contrast: ${contrast(primary)};
+    --ion-color-primary-contrast-rgb: 255,255,255;
+    --ion-color-primary-shade:  ${Color(primary).darken(shadeRatio)};
+
+    // omitted other styles, see full source code
+`;
 }
